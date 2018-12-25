0bc07a7cb02e1a1a21457f281ce178217f2e24e7
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-12-25 17:23:51 +0900

    Wait for Helm install to complete before usage

--- lib/update/component_installer.rb
+++ lib/update/component_installer.rb
@@ -2,6 +2,8 @@ require 'update/charts'
 
 module Update
   class ComponentInstaller
+    MAX_ATTEMPTS = 100
+
     class << self
       def call(logger)
         configure_helm(logger) if helm_unconfigured?(logger)
@@ -35,13 +37,14 @@ module Update
       end
 
       def helm_command(chart)
-        values = chart.fetch(:params).map {|k,v| "#{k}=#{v}"}.join("--values ")
+        values = chart.fetch(:params).map { |k, v| "#{k}=#{v}" }.join("--values ")
         [
-          "helm install",
+          "helm upgrade #{chart.fetch(:name)}",
           "#{chart.fetch(:channel)}/#{chart.fetch(:name)}",
+          '--install',
           "--version #{chart.fetch(:version)}",
           values
-        ].join(' ')
+        ].compact.join(' ')
       end
 
       def helminate(logger)
@@ -69,12 +72,20 @@ module Update
         _version, status = Open3.capture2e('helm version')
         configured       = status.to_i.zero?
         configured ? logger.debug("Helm is configured") : logger.debug("Helm is not configured")
-        configured
+        !configured
       end
 
       def configure_helm(logger)
         logger.debug("Configuring Helm")
-        status = Open3.popen2e('helm init') do |_, stdout_stderr, wait_thread|
+        status = Open3.popen2e('kubectl create serviceaccount --namespace kube-system tiller') do |_, stdout_stderr, wait_thread|
+          while (line = stdout_stderr.gets) do
+            logger.debug line.chomp
+          end
+
+          wait_thread.value
+        end
+        abort("Error creating Helm service account") unless status.to_i.zero?
+        status = Open3.popen2e('kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller') do |_, stdout_stderr, wait_thread|
           while (line = stdout_stderr.gets) do
             logger.debug line.chomp
           end
@@ -82,7 +93,29 @@ module Update
           wait_thread.value
         end
 
-        status
+        abort("Error configuring Helm service account") unless status.to_i.zero?
+
+        Open3.popen2e('helm init --service-account tiller') do |_, stdout_stderr, wait_thread|
+          while (line = stdout_stderr.gets) do
+            logger.debug line.chomp
+          end
+
+          wait_thread.value
+        end
+
+        abort("Timed out waiting for Helm to install") unless wait_for_helm(logger)
+      end
+
+      def wait_for_helm(logger, attempt = 0)
+        return false if attempt == MAX_ATTEMPTS
+
+        if helm_unconfigured?(logger)
+          attempt += 1
+          sleep(3)
+          wait_for_helm(logger, attempt)
+        else
+          true
+        end
       end
     end
   end
