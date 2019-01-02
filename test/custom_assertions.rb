043364c4fad3dffcdb2d3d618e851a4bead143e1
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2019-01-02 17:10:24 +0900

    Wait up to five minutes for a Pod to start during test

--- test/custom_assertions.rb
+++ test/custom_assertions.rb
@@ -1,13 +1,24 @@
 module CustomAssertions
-  def assert_pod_running(name, namespace = 'eks-in-a-box', msg = nil)
-    pod = `kubectl -n #{namespace} get pod | grep #{name}`.chomp
+  POD_RUNNING_MAX_ATTEMPTS = 100
+  POD_RUNNING_INTERVAL     = 3_000
+
+  def assert_pod_running(name, namespace = 'eks-in-a-box', attempt = 0, msg = nil)
+    pod    = `kubectl -n #{namespace} get pod | grep #{name}`.chomp
     status = pod.split(' ')[2]
-    msg = ''
+
     if status.nil?
-      msg = message(msg) { "Expected Pod '#{name}' to be running but it does not exist in namespace '#{namespace}'"}
+      msg = message(msg) { "Expected Pod '#{name}' to be running but it does not exist in namespace '#{namespace}'" }
     else
       msg = message(msg) { "Expected Pod '#{name}' to have status of 'Running' but was '#{status}'" }
     end
-    assert(status == 'Running', msg)
+
+    if status == 'Running'
+      assert true
+    elsif attempt < POD_RUNNING_MAX_ATTEMPTS
+      sleep POD_RUNNING_INTERVAL
+      assert_pod_running(name, namespace, attempt + 1, msg)
+    else
+      flunk msg
+    end
   end
 end
